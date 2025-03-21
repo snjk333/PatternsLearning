725e943fe08afaa91906bdd81def9508061bfe44
Oleksandr <kulbitsanya0@gmail.com>
2025-03-21 20:49:30 +0100

    Singleton Pattern Example

---
+++ src/main/java/com/oleksandr/Creational/Singleton/MainClass.java
@@ -0,0 +1,13 @@
+package com.oleksandr.Creational.Singleton;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        if(s1 == s2){
+            System.out.println("s1 and s2 have same address");
+        }
+    }
+}
